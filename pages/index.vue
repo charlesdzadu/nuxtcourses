b29ac9fa5ee0638f13a24943f51ace592d5ed458
charlesdzadu <dkccharlie@gmail.com>
2022-06-10 21:49:53 +0000

    contacts deploy

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,60 @@
   <div>
     <router-link to="/collections">Collection</router-link>
     <h1 class="text-3xl">Saluto</h1>
+    <p>
+      <button @click="readContacts">Read Contacts</button>
+    </p>
+
+    <pre id="log"></pre>
   </div>
 </template>
 
 <script>
 export default {
-  layout: "blog"
+  layout: "blog",
+  methods: {
+    readContacts() {
+      var api = (navigator.contacts || navigator.mozContacts);
+
+      if (api && !!api.select) { // new Chrome API
+        api.select(['name', 'email'], { multiple: true })
+          .then(function (contacts) {
+            consoleLog('Found ' + contacts.length + ' contacts.');
+            if (contacts.length) {
+              consoleLog('First contact: ' + contacts[0].name + ' (' + contacts[0].email + ')');
+            }
+          })
+          .catch(function (err) {
+            consoleLog('Fetching contacts failed: ' + err.name);
+          });
+
+      } else if (api && !!api.find) { // old Firefox OS API
+        var criteria = {
+          sortBy: 'familyName',
+          sortOrder: 'ascending'
+        };
+
+        api.find(criteria)
+          .then(function (contacts) {
+            consoleLog('Found ' + contacts.length + ' contacts.');
+            if (contacts.length) {
+              consoleLog('First contact: ' + contacts[0].givenName[0] + ' ' + contacts[0].familyName[0]);
+            }
+          })
+          .catch(function (err) {
+            consoleLog('Fetching contacts failed: ' + err.name);
+          });
+
+      } else {
+        consoleLog('Contacts API not supported.');
+      }
+    },
+
+    consoleLog(data) {
+      var logElement = document.getElementById('log');
+      logElement.innerHTML += data + '\n';
+    }
+
+  }
 }
 </script>
